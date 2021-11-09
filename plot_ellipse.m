50d27fdc14bd21937ffc60b9477562d0755efb20
Jared Blanchard <jared711@stanford.edu>
2021-11-09 10:01:33 -0800

    docs for plot_ellipse.m

--- plot_ellipse.m
+++ plot_ellipse.m
@@ -1,21 +1,27 @@
-function [h] = plot_ellipse(Mu,Sigma,P,a)
-%PLOT_ELLIPSE Summary of this function goes here
+function [h] = plot_ellipse(Mu,Sigma,p,a)
+%PLOT_ELLIPSE Plots a 2D ellipse or 3D ellipsoid centered at Mu and defined
+%by Sigma where points x on the boundary of the ellipse are defined by
+%x'*inv(Sigma)*x < sqrt(-2*log(1-p))
 % 
 % [OUTPUTARGS] = PLOT_ELLIPSE(mu,Sigma,P)
 % 
 % Inputs: 
+%           Mu (2x1) or (3x1) mean vector
+%           Sigma (2x2) or (3x3) covariance matrix
+%           p = (scalar) probability of ellipsoid
+%           a = (integer) color index
 % 
 % Outputs: 
+%           h (figure handle) output figure
 % 
 % See also: 
 
 % Author: Jared Blanchard 	Date: 2021/04/24 15:49:56 	Revision: 0.1 $
 if nargin < 4;  a = 1;      end
-if nargin < 3;  P = 0.3935; end
+if nargin < 3;  p = 0.3935; end
 if isrow(Mu);   Mu = Mu';   end
 
-
-r = sqrt(-2*log(1-P));
+r = sqrt(-2*log(1-p));
 
 if length(Mu) == 2
     theta = linspace(0,2*pi);
@@ -39,8 +45,7 @@ elseif length(Mu) == 3
 %     plot3(cx,cy,cz,'.')
     h = surf(cx,cy,cz,a*ones(size(X)),'EdgeColor','none','HandleVisibility','off');%,'FaceColor','c');
 else
-    warning("Mu should be 3x1")
+    warning("Mu should be 2x1 or 3x1")
 end
-
 
 end
